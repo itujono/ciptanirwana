879ee07589d62588e8fbb6f8c2fa6b378fdbbe39
andhan <magicwarms@gmail.com>
2018-09-27 17:00:28 +0700

    Yihaa Yahuu

--- application/controllers/Eed.php
+++ application/controllers/Eed.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Cipta extends Frontend_Controller {
+class Eed extends Frontend_Controller {
 
 	public function __construct (){
         parent::__construct();
